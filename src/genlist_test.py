2128098d3345045411b6dc85bc4b9eb3bca76ee5
Prathanporn <wo.prathanporn_st@tni.ac.th>
2017-02-03 03:09:29 +0000

    C5-generate_list v2 tested

--- src/genlist_test.py
+++ src/genlist_test.py
@@ -7,5 +7,6 @@ sys.path.append(cwd)
 # print(sys.path)
 
 # Test the module:generate_list
-from generate_list import printIt
-printIt()
+for x in xrange(1000):
+    from generate_list import printIt
+    printIt()
