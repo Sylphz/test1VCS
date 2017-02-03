77e9900eefd3a2b22d1f0accd127b15f0a9f30eb
Prathanporn <wo.prathanporn_st@tni.ac.th>
2017-02-03 02:15:12 +0000

    C2 - Error(s) corrected

---
+++ src/generate_list.py
@@ -0,0 +1,22 @@
+import random
+def generate_list():
+    alist = [x for x in range(random.radint(-10,10))]
+    return alist
+
+"""
+print a generate_list
+"""
+
+def printIt():
+    print(generate_list())
+    
+def main():
+    printIt()
+    
+"""
+If this script file is called, it will run main() directly
+"""
+
+if_name_=='_main_'
+    print("Test printIt():")
+    main()
